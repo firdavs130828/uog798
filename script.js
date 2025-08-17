db623d5a7f4d54ca68cd42cb60c546e4a5340055
firdavs130828 <arabovfirdavs7@gmail.com>
2025-08-17 23:18:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let raqamlar=[65,32,8,2,23,98,9,55,7,5,32,5623,92,3];
+let juft=[];
+let toq=[];
+for(let i=0; i<raqamlar.length; i++){
+    if(raqamlar[i]%2==0){
+        juft.push(raqamlar [i])
+    } else{
+                toq.push(raqamlar [i])
+    }
+}
+console.log(raqamlar, "hamma raqamlar");
+console.log(juft, "juft sondagi raqamlar");
+console.log(toq, "toq sondagi raqamlar");
+
+
+
+
